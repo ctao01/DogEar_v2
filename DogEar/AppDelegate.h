b2ef07d56e615d4d76e668b46b6e70b0f0fed720
joycytao <ctao01.poly@gmail.com>
2012-11-26 20:33:25 -0600

    Initial Commit

---
+++ DogEar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DogEar
+//
+//  Created by Joy Tao on 11/26/12.
+//  Copyright (c) 2012 Joy Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
