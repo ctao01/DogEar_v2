11ab41418c3df4bdb892324d4c3b6f9ae6fae24e
Joy Tao <ctao01@students.poly.edu>
2012-12-02 01:14:53 -0600

    JT - Search Feature

--- DogEar/Classes/ViewControllers/de_BrowseTableViewController.m
+++ DogEar/Classes/ViewControllers/de_BrowseTableViewController.m
@@ -13,15 +13,22 @@
 #import "de_BrowseTableViewController.h"
 #import "de_ListTableViewController.h"
 #import "de_FlaggedListViewController.h"
-#import "DogEarObject.h"
+#import "de_PhotoViewController.h"
 
 @interface de_BrowseTableViewController ()
 {
     NSArray * categories;
+    
 }
+@property (nonatomic , strong) NSArray * allItems;
+@property (nonatomic , strong) NSArray * searchItems;
+@property (nonatomic , strong) UISearchDisplayController * vcSearchDispay;
 @end
 
 @implementation de_BrowseTableViewController
+@synthesize allItems, searchItems;
+@synthesize vcSearchDispay;
+
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -44,7 +51,11 @@
     searchBar.tintColor = [UIColor colorWithRed:129.0/255.0f green:129.0/255.0f blue:130.0/255.0f alpha:.80];
     searchBar.showsSearchResultsButton = YES;
     self.tableView.tableHeaderView = searchBar;
-
+    
+    self.vcSearchDispay = [[UISearchDisplayController alloc]initWithSearchBar:searchBar contentsController:self];
+    self.vcSearchDispay.searchResultsDataSource = self;
+    self.vcSearchDispay.searchResultsDelegate = self;
+    self.vcSearchDispay.delegate = self;
 }
 
 - (void) viewWillAppear:(BOOL)animated
@@ -97,19 +108,65 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - 
+
+- (NSMutableArray *) allItems
+{
+    NSMutableArray * collections = [[NSMutableArray alloc]init];
+    
+    for (int c = 0; c < [categories count]; c++)
+    {
+        NSData * data = [[[NSUserDefaults standardUserDefaults]objectForKey:@"BKDataCollections"] objectForKey:[categories objectAtIndex:c]];
+        NSMutableArray * decodedCollections = [NSMutableArray arrayWithArray:[NSKeyedUnarchiver unarchiveObjectWithData: data]];
+        NSArray * copyCollections = [[NSArray arrayWithArray:decodedCollections] copy];
+        
+        for (DogEarObject * object in copyCollections) [collections addObject:object];
+    }
+    return collections;
+}
+
+- (void) filterContentForSearchText:(NSString*)searchText andScope:(NSString*)scope
+{
+    self.allItems = [[NSArray alloc]initWithArray:[self allItems]];   
+    NSPredicate * resultPredict = [NSPredicate predicateWithFormat:@"SELF.title contains[cd] %@",searchText];
+    self.searchItems = [self.allItems filteredArrayUsingPredicate:resultPredict];
+}
+
+#pragma mark - UISearchDisplay Delegate
+
+- (BOOL) searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString andScope:nil];
+    return YES;
+}
+
+- (BOOL) searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
+{
+    [self filterContentForSearchText:[controller.searchBar text] andScope:nil];
+    return YES;
+    
+}
+
 #pragma mark - Table View Data Source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    if ([tableView isEqual: self.vcSearchDispay.searchResultsTableView])
+        return 1;
+    else return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    if (section == 0) return 1;
-    else if (section == 1) return [categories count];
+    if ([tableView isEqual: self.vcSearchDispay.searchResultsTableView])
+        return [self.searchItems count];
     
-    else return 0;
+    else
+    {
+        if (section == 0) return 1;
+        else if (section == 1) return [categories count];
+        else return 0;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -119,11 +176,14 @@
     if (cell == nil) cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     // Configure the cell...
+    if ([tableView isEqual:self.vcSearchDispay.searchResultsTableView])
+        cell.textLabel.text = [(DogEarObject*)[self.searchItems objectAtIndex:indexPath.row] title];
     
-    if (indexPath.section == 0) cell.textLabel.text = @"Flagged Dog Ears";
-    else if (indexPath.section == 1 )
-        cell.textLabel.text = [categories objectAtIndex:indexPath.row];
-    
+    else {
+        if (indexPath.section == 0) cell.textLabel.text = @"Flagged Dog Ears";
+        else if (indexPath.section == 1 )
+            cell.textLabel.text = [categories objectAtIndex:indexPath.row];
+    }
     return cell;    
 }
 
@@ -132,37 +192,48 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     //JT-TODO: Read Collections of Selected Row
-    if (indexPath.section == 0)
+    
+    if ([tableView isEqual:self.vcSearchDispay.searchResultsTableView])
     {
-        NSMutableArray * collections = [[NSMutableArray alloc]init];
-        
-        for (int c = 0; c < [categories count]; c++)
-        {
-            NSData * data = [[[NSUserDefaults standardUserDefaults]objectForKey:@"BKDataCollections"] objectForKey:[categories objectAtIndex:c]];
-            NSMutableArray * decodedCollections = [NSMutableArray arrayWithArray:[NSKeyedUnarchiver unarchiveObjectWithData: data]];
-            NSArray * copyCollections = [[NSArray arrayWithArray:decodedCollections] copy];
-            
-            for (DogEarObject * object in copyCollections)
-            {
-                if (object.flagged != nil)  [collections addObject:object];
-            }
-        }
-        NSLog(@"%i",[collections count]);
-        
-        de_FlaggedListViewController * vc = [[de_FlaggedListViewController alloc]initWithStyle:UITableViewStyleGrouped];
-        vc.flaggedCollections = collections;
+        DogEarObject * selectedDogEar = (DogEarObject*)[self.searchItems objectAtIndex:indexPath.row];
+        UIImage * image = [UIImage imageWithData:[NSData dataWithContentsOfFile:selectedDogEar.imagePath]];
+        de_PhotoViewController * vc = [[de_PhotoViewController alloc]initWithImage:[[UIImage alloc] initWithCGImage:image.CGImage scale:1.0 orientation:[selectedDogEar.imageOrientation integerValue] ] andExistingDogEar:selectedDogEar];
         [self.navigationController pushViewController:vc animated:YES];
     }
-    else if (indexPath.section == 1)
+    else
     {
-        NSString * keyString = [categories objectAtIndex:indexPath.row];
-        NSData * data = [[[NSUserDefaults standardUserDefaults]objectForKey:@"BKDataCollections"] objectForKey:keyString];
-        NSMutableArray * decodedCollections = [NSMutableArray arrayWithArray:[NSKeyedUnarchiver unarchiveObjectWithData: data]];
-        
-        de_ListTableViewController * vc = [[de_ListTableViewController alloc]init];
-        [self.navigationController pushViewController:vc animated:YES];
-        vc.navigationItem.title = [NSString stringWithFormat:@"Category:%@",keyString];
-        vc.collections = decodedCollections;
+        if (indexPath.section == 0)
+        {
+            NSMutableArray * collections = [[NSMutableArray alloc]init];
+            
+            for (int c = 0; c < [categories count]; c++)
+            {
+                NSData * data = [[[NSUserDefaults standardUserDefaults]objectForKey:@"BKDataCollections"] objectForKey:[categories objectAtIndex:c]];
+                NSMutableArray * decodedCollections = [NSMutableArray arrayWithArray:[NSKeyedUnarchiver unarchiveObjectWithData: data]];
+                NSArray * copyCollections = [[NSArray arrayWithArray:decodedCollections] copy];
+                
+                for (DogEarObject * object in copyCollections)
+                {
+                    if (object.flagged != nil)  [collections addObject:object];
+                }
+            }
+            NSLog(@"%i",[collections count]);
+            
+            de_FlaggedListViewController * vc = [[de_FlaggedListViewController alloc]initWithStyle:UITableViewStyleGrouped];
+            vc.flaggedCollections = collections;
+            [self.navigationController pushViewController:vc animated:YES];
+        }
+        else if (indexPath.section == 1)
+        {
+            NSString * keyString = [categories objectAtIndex:indexPath.row];
+            NSData * data = [[[NSUserDefaults standardUserDefaults]objectForKey:@"BKDataCollections"] objectForKey:keyString];
+            NSMutableArray * decodedCollections = [NSMutableArray arrayWithArray:[NSKeyedUnarchiver unarchiveObjectWithData: data]];
+            
+            de_ListTableViewController * vc = [[de_ListTableViewController alloc]init];
+            [self.navigationController pushViewController:vc animated:YES];
+            vc.navigationItem.title = [NSString stringWithFormat:@"Category:%@",keyString];
+            vc.collections = decodedCollections;
+        }
     }
 }
 
